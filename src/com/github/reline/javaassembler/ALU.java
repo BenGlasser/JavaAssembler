803a94fc6337d2eb197f1d54fdc0c10607774209
Reline <nathanielreline@gmail.com>
2016-03-02 11:48:22 -0700

    Setup

---
+++ src/com/github/reline/javaassembler/ALU.java
@@ -0,0 +1,23 @@
+package com.github.reline.javaassembler;
+
+public class ALU {
+    public void ADD() {
+
+    }
+
+    public void SUBTRACT() {
+
+    }
+
+    public void AND() {
+
+    }
+
+    public void OR() {
+
+    }
+
+    public void NOT() {
+
+    }
+}
